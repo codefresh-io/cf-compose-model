9016c96a506c0d85408081f36fed8dfa93f581ef
Oleg Sucharevich <olegs@codefresh.io>
2017-03-07 16:40:53 +0200

    Compose v1 supported , options to upgrade from compose v1 to compose v2 - not fully tested yet

--- test.js
+++
@@ -1,3 +0,0 @@
-const YAML = require('yamljs');
-const yaml = YAML.load('/Users/oleg/workspace/codefresh/cf-compose-model/lib/model/tests/ComposeV1/ex2.yaml');
-console.log(YAML.stringify(yaml, 4, 2));
