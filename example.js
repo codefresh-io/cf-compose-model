04abee8b9adb4c3f192262201ec29ead7f351330
Oleg Sucharevich <olegs@codefresh.io>
2017-03-16 23:35:55 +0200

    fix lint, added examples

--- example.js
+++ example.js
@@ -3,14 +3,14 @@
 const path         = require('path');
 const CM           = require('./');
 const ComposeModel = CM.ComposeModel;
-const Service      = CM.components.Service;
+const Service      = CM.components.Service; // jshint ignore:line
 
 /*Examples*/
 
 const paths = [
-    // './lib/model/tests/ComposeV1/ex1.yaml',
+    './lib/model/tests/ComposeV1/ex1.yaml',
     './lib/model/tests/ComposeV2/ex2.yaml',
-    // './lib/model/parsers/tests/yamls/ComposeV1/ex1.image.yaml'
+    './lib/model/parsers/tests/yamls/ComposeV1/ex1.image.yaml'
 ];
 paths.map((location) => {
 
@@ -25,36 +25,43 @@ paths.map((location) => {
             return compose.getErrorsAndWornings();
         })
         .then((errors) => {
-            console.log(errors)
-        })
-    // console.log('Parsed');
-    // console.log('Show warnings');
-    // console.log(compose.getWarnings());
-    //
-    // console.log('Fix warnings');
-    // compose.fixWarnings();
-    //
-    // console.log('Show warnings after fix');
-    // console.log(compose.getWarnings());
-    //
-    // console.log('Translate with default translator');
-    // console.log(compose.translate());
+            console.log(errors);
+        });
 });
 
-// console.log(`Example starting from scratch`);
-// console.log(`#############################`);
-//
-// const cm      = new ComposeModel();
-// const service = new Service('os')
-//     .setImage('ubuntu')
-//     .addPort('80:80')
-//     .addVolume('./app:/core')
-//     .addEnvironmentVariable('TIME', Date.now())
-//     .setAdditionalData('dockerfile', './Dockerfile');
-// cm.addService(service);
-// const translated = cm.translate(CM.translators.ComposeV1);
-// console.log(translated);
-// console.log(cm.getWarnings());
-// cm.fixWarnings();
-// console.log(cm.getWarnings());
-// console.log(cm.translate(CM.translators.ComposeV1));
+console.log(`Example starting from scratch`);
+console.log(`#############################`);
+
+const cm      = new ComposeModel();
+const service = new Service('os')
+    .setImage('ubuntu')
+    .addPort('80:80')
+    .addVolume('./app:/core')
+    .addEnvironmentVariable('TIME', Date.now())
+    .setAdditionalData('dockerfile', './Dockerfile');
+cm.addService(service);
+cm.translate(CM.translators.ComposeV1)
+    .then(translated => {
+        console.log(translated);
+    })
+    .then(() => {
+        return cm.getWarnings();
+    })
+    .then(warnings => {
+        console.log(warnings);
+    })
+    .then(() => {
+        return cm .fixWarnings();
+    })
+    .then(() => {
+        return cm.getWarnings();
+    })
+    .then(warnings => {
+        console.log(warnings);
+    })
+    .then(() => {
+        return cm.translate(CM.translators.ComposeV1);
+    })
+    .then(translated => {
+        console.log(translated);
+    });
