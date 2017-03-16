9b93bf5923e0d75ebe0b3fad037b60da2f65c0e5
Oleg Sucharevich <olegs@codefresh.io>
2017-03-16 14:59:53 +0200

    moved to promises

--- example.js
+++ example.js
@@ -9,7 +9,7 @@ const Service      = CM.components.Service;
 
 const paths = [
     './lib/model/tests/ComposeV1/ex1.yaml',
-    './lib/model/tests/ComposeV2/ex1.yaml',
+    './lib/model/tests/ComposeV2/ex2.yaml',
 ];
 
 paths.map((location) => {
@@ -20,35 +20,39 @@ paths.map((location) => {
 
     location = path.resolve(__dirname, location);
     console.log(`Loaded path ${location}`);
-    const compose = ComposeModel.load(location);
-    console.log('Parsed');
-    console.log('Show warnings');
-    console.log(compose.getWarnings());
-
-    console.log('Fix warnings');
-    compose.fixWarnings();
-
-    console.log('Show warnings after fix');
-    console.log(compose.getWarnings());
-
-    console.log('Translate with default translator');
-    console.log(compose.translate());
+    ComposeModel.load(location)
+        .then((compose) => {
+            return compose.translate()
+        })
+        .then(console.log);
+    // console.log('Parsed');
+    // console.log('Show warnings');
+    // console.log(compose.getWarnings());
+    //
+    // console.log('Fix warnings');
+    // compose.fixWarnings();
+    //
+    // console.log('Show warnings after fix');
+    // console.log(compose.getWarnings());
+    //
+    // console.log('Translate with default translator');
+    // console.log(compose.translate());
 });
 
-console.log(`Example starting from scratch`);
-console.log(`#############################`);
-
-const cm      = new ComposeModel();
-const service = new Service('os')
-    .setImage('ubuntu')
-    .addPort('80:80')
-    .addVolume('./app:/core')
-    .addEnvironmentVariable('TIME', Date.now())
-    .setAdditionalData('dockerfile', './Dockerfile');
-cm.addService(service);
-const translated = cm.translate(CM.translators.ComposeV1);
-console.log(translated);
-console.log(cm.getWarnings());
-cm.fixWarnings();
-console.log(cm.getWarnings());
-console.log(cm.translate(CM.translators.ComposeV1));
+// console.log(`Example starting from scratch`);
+// console.log(`#############################`);
+//
+// const cm      = new ComposeModel();
+// const service = new Service('os')
+//     .setImage('ubuntu')
+//     .addPort('80:80')
+//     .addVolume('./app:/core')
+//     .addEnvironmentVariable('TIME', Date.now())
+//     .setAdditionalData('dockerfile', './Dockerfile');
+// cm.addService(service);
+// const translated = cm.translate(CM.translators.ComposeV1);
+// console.log(translated);
+// console.log(cm.getWarnings());
+// cm.fixWarnings();
+// console.log(cm.getWarnings());
+// console.log(cm.translate(CM.translators.ComposeV1));
